86b7b655fd8180ab2be925306df9f19b6e55aca2
Aline <Aline@ALINE>
2024-11-02 10:26:40 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppPedro/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiAppPedro
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MauiAppPedro/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiAppPedro
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
